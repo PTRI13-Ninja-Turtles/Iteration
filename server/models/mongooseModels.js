a0dce4acd40ff9fee61a8e82dbab20a7755e46c8
Moises Gomez <moisesgomez@Juanas-Air.lan>
2024-01-13 11:07:10 -0800

    created the connection to the atlas db

--- server/models/mongooseModels.js
+++ server/models/mongooseModels.js
@@ -1,2 +1,12 @@
 const mongoose = require('mongoose');
-const MONGO_URI = ''
+const MONGO_URI = 'mongodb+srv://moisesgomezr9:L37udLyPOFIfqRtM@scratch-project.j3hrygw.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(MONGO_URI, {
+    // options for the connect method to parse the URI
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // sets the name of the DB that our collections are part of
+    dbName: 'scratch project'
+  })
+    .then(() => console.log('Connected to Mongo DB.'))
+    .catch(err => console.log(err));
+
