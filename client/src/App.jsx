cd8d44ad69f389edfb2134934b8df309a9794769
Moises Gomez <moisesgomez@Juanas-Air.lan>
2024-01-15 14:09:17 -0800

    created NavigationInnerBar component file and imported it into the App jsx file to render

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { NavigationInnerBar } from './Components/NavigationInnerBar.jsx';
+
+
+const App = () => {
+  return (<NavigationInnerBar />);
+};
+
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
+import App from "./App.jsx";
 
 const root = createRoot(document.querySelector('#root'));
-root.render ();
+root.render (<App/>);
