66722b7a8fac12aaf1511cbf82ca9b298630536a
Matt Allen <matt.w.allen@gmail.com>
2024-01-15 14:25:39 -0800

    updated users controller to inclue income, expense, and retirement contributions. Added StateYTDCalc function to CalcController.

--- server/Controllers/CalcController.js
+++ server/Controllers/CalcController.js
@@ -1,25 +1,54 @@
+/* 
+res.locals.stateTables <-- and array of objects with table information.
+Each object element consists of {
+                                    state_name:
+                                    filing_status:
+                                    income_range_low:
+                                    income_range_high:
+                                    tax_rate:
+                                 }
+
+Still need to fetch YTD & transaction data
+*/ 
 
 
-const calc = {}
+
+const calc = {};
 
 // retreive rates prior to invoking this middleware
 calc.stateYTDCalc = (req, res, next) => {
-    // need to know:
-        // User
-            // state 
-            // filing status
-            // YTD Net Earnings
 
-    const location = res.locals.user.state
-    const filingStatus = res.locals.user.filingStatus
-    const netIncome = Number(res.locals.user.estimatedIncome) - Number(res.locals.user.businessExpenses)
+  const YTD = res.locals.estimatedIncome - (res.locals.businessExpenses + res.locals.preTaxRetirementContributions); // <-- property needs to be added
+  const bracketLow = res.locals.stateTables.forEach((ele) => {ele[income_range_low]});
+  const bracketHigh = res.locals.stateTables.forEach((ele) => {ele[income_range_high]});
+  const rates = res.locals.stateTables.forEach((ele) =>{ele[tax_rate]});
+  let taxesOwed = 0;
 
-    const filingBrackets = res.locals.user.brackets
+// edge case, flat rate
 
-   // yadda yadda ya
-   // output is a number
+  if (bracketHigh[0] === 999999999){
+    taxesOwed = YTD * rates[0];
+  } else {
+    for (let i=0; i<brackets.length; i++){
+      const min = bracketLow[i];
+      const max = bracketLow[i+1];
+      const currentRate = rates[i];
 
+      if (max === undefined){
+        taxesOwed += ((YTD - min) * currentRate);
+      } else if (YTD <= max){
+        taxesOwed += ((YTD - min) * currentRate);
+        break;
+      } else {
+        taxesOwed += ((max-min) * currentRate);
+      }
+    }
+  }
+
+  res.locals.taxesOwed.state = taxesOwed;
+  return next();
 }
+
 
 
 module.exports = calc;
