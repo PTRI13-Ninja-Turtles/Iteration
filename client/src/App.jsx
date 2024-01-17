e95c4c48a36ae1957a4820240a93bdf3d6eb571e
GregSilvia <iamgregsilvia@gmail.com>
2024-01-16 17:40:04 -0800

    login/signup pages done, need additional styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
-import LandingPageContainer from './Containers/LandingPageContainer.jsx';
+import React from "react";
+import LandingPageContainer from "./containers/LandingPageContainer.jsx";
+import LoginPageContainer from "./containers/LoginPageContainer.jsx";
+import SignupPageContainer from "./containers/SignupPageContainer.jsx";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 
 const App = () => {
-  return (<LandingPageContainer />);
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPageContainer />} />
+          <Route path="/login" element={<LoginPageContainer />} />
+          <Route path="/signup" element={<SignupPageContainer />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
