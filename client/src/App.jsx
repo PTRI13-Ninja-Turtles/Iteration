9bb4acb43afb46005c768e38d7cc42027d56348f
GregSilvia <iamgregsilvia@gmail.com>
2024-01-18 13:14:53 -0800

    userdashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,19 @@
-import React from "react";
-import LandingPageContainer from "./containers/LandingPageContainer.jsx";
-import LoginPageContainer from "./containers/LoginPageContainer.jsx";
-import SignupPageContainer from "./containers/SignupPageContainer.jsx";
-import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import React from 'react';
+import MainPageContainer from './Containers/MainPageContainer.jsx';
+import LoginPageContainer from './Containers/LoginPageContainer.jsx';
+import SignupPageContainer from './Containers/SignupPageContainer.jsx';
+import UserDashboardContainer from './Containers/UserDashboardContainer.jsx';
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <div className="app">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<LandingPageContainer />} />
+          <Route path="/" element={<MainPageContainer />} />
           <Route path="/login" element={<LoginPageContainer />} />
           <Route path="/signup" element={<SignupPageContainer />} />
+          <Route path="/dashboard" element={<UserDashboardContainer />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ client/src/Containers/MainPageContainer.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { NavigationInnerBar } from '../components/MainPageComponents/NavigationInnerBar.jsx';
+import { HeroSection } from '../components/MainPageComponents/HeroSectionComponent.jsx';
+import { MainSectionComponent } from '../components/MainPageComponents/MainSectionComponent.jsx';
+import { Box } from '@mui/material';
+import { StayInformedComponent } from '../components/MainPageComponents/StayInformedComponent.jsx';
+
+const MainPageContainer = () => {
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      width="100%"
+      className='LandingPageContainer'
+      maxWidth="1300px" // Set your desired max-width value
+      margin="0 auto"  // Center the container horizontally
+      overflow="hidden"
+      
+    >
+      <NavigationInnerBar />
+      <HeroSection />
+      <MainSectionComponent />
+      <StayInformedComponent />
+    </Box>
+  );
+};
+
+export default MainPageContainer;
