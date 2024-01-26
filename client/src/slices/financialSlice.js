bc8d88a43fbb9ea0da38b2f9190f14807c0158cf
James Shea <jshea915@gmail.com>
2024-01-26 16:34:56 -0700

    slices finished and submission functionality broken out - committing before testing to see if local state can be redplaced with redux state

--- client/src/slices/financialSlice.js
+++ client/src/slices/financialSlice.js
@@ -1,18 +1,26 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { transactionOwed } from '../../../server/Controllers/CalcController';
 
-// 
+// Copied initial state details from dashboard container.
 
 const initialState = {
-// ORIGINAL STATE PROPERTIES:
-//   amount: 0,
-//   source: '',
-//   timestamp: '',
-//   medicareTax: 0,
-//   stateTax: 0,
-//   ssiTax: 0,
-//   federalTax: 0,
-  earningData: {},
-  deductionData: {},
+  earningData: {
+    amount: 0,
+    source: '',
+    timestamp: '',
+    type: 'earning',
+    medicareTax: 0,
+    stateTax: 0,
+    ssiTax: 0,
+    federalTax: 0,
+  },
+  deductionData: {
+    amount: 0,
+    source: '',
+    timestamp: '',
+    type: 'deduction',
+  },
+  transactions: [],
   status: 'pending',
   error: null
 };
@@ -21,16 +29,63 @@ const initialState = {
 // Action types: 'deduction', 'earning'
 
 // Async Thunk functions for fetch requests
-  // post request add earning data to database 
-  // post request add deduction data to database
+// post request add earning data to database 
+// post request add deduction data to database
 
-export const postEarning = createAsyncThunk({
 
-});
+// In the original dashboard the postEarning also updates the pieChart updates the transaction array and then sets the transactons.
+// Original code also pulls out the state.tax from the userTransaction data. This is used in updating pieChart
+export const postEarning = createAsyncThunk(
+  'postEarning',
+  async (earningData, {getState, rejectWithValue}) => {
+    const token = localStorage.getItem('token');
+    try {
+      const response = await fetch('http://localhost:3000/transaction', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify(earningData),
+      });
+      // Check response.
+      if(!response.ok) throw new Error('Network response was not ok');
+      // Convert data to JSON.
+      const data = await response.json();
+      console.log('JSON response from post request within postEarning', data);
+      return data;
+    } catch (error) {
+      console.log('error from post request within post earning', error);
+      return rejectWithValue(error.message);
+    }
+  }
+);
 
-export const postDeduction = createAsyncThunk({
-
-});
+export const postDeduction = createAsyncThunk(
+  'postDeduction',
+  async (deductionData, {getState, rejectWithValue}) => {
+    const token = localStorage.getItem('token');
+    try {
+      const response = await fetch('http://localhost:3000/transaction', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify(deductionData),
+      });
+      // Check response.
+      if(!response.ok) throw new Error('Network response was not ok');
+      // Convert data to JSON.
+      const data = await response.json();
+      console.log('JSON response from post request within postDeduction', data);
+      return data;
+    } catch (error) {
+      console.log('error from post request within post deduction', error);
+      return rejectWithValue(error.message);
+    }
+  }
+);
 
 //financialSlice function
 const financialSlice = createSlice({
@@ -47,26 +102,54 @@ const financialSlice = createSlice({
   extraReducers: (builder) => {
     builder
       .addCase(postEarning.pending, (state) => {
-        state.status = 'loading'
+        state.status = 'loading';
       })
       .addCase(postEarning.fulfilled, (state, action) => {
-        state.status = 'success'
+        state.status = 'success';
+        // Once earning is posted to database need to update transactions state from data returned.
+
+        // Accesses the payload object which has the data necessary to update the transaction state.
+        // This is coped from the existing code that creates a newEarningTransaction with somee optimizations.
+        const newTransactions = action.payload.userTransactionData.incomes.map((earning) => ({
+          id: state.transactions.length + 1,
+          description: `Earning | ${earning.source}`,
+          amount: `+$${earning.amount.toFixed(2)}`,
+          medicareTax: `Medicare Tax | ${earning.transMedicare.toFixed(2)}`,
+          stateTax: `State Tax | ${earning.transState.toFixed(2)}`,
+          ssiTax: `SSI Tax | ${earning.transSSI.toFixed(2)}`,
+          federalTax: `Federal Tax | ${earning.transFed.toFixed(2)}`,
+          // timestamp: new Date().toISOString(), // to generate timestamp on the frontend?
+        }));
+        // Use spread operator create new transaction array with newEarningTransaction sequentially at the end of the array.
+        state.transactions = [...state.transactions, newTransactions];
       })
       .addCase(postEarning.rejected, (state, action) => {
-        state.status = 'failed'
-        state.error = `Error occured in financialSlice postEarningThunk: ${action.payload}`
+        state.status = 'failed';
+        state.error = `Error occured in financialSlice postEarningThunk: ${action.payload}`;
       })
       .addCase(postDeduction.pending, (state) => {
-        state.status = 'loading'
+        state.status = 'loading';
       })
       .addCase(postDeduction.fulfilled, (state, action) => {
-        state.status = 'success'
+        state.status = 'success';
+        // Once deduction is posted to database need to update transactions state from data returned.
+
+        // Accesses the payload object which has the data necessary to update the transaction state.
+        // This is coped from the existing code that created newExpenseTransaction with somee optimizations.
+        const newTransactions = action.payload.userTransactionData.expenses.map((deduction) => ({
+          id: state.transactions.length + 1,
+          description: `Deduction | ${deduction.source}`,
+          amount: `+$${deduction.amount.toFixed(2)}`,
+          // timestamp: new Date().toISOString(), // to generate timestamp on the frontend?
+        }));
+        // Use spread operator create new transaction array with newEarningTransaction sequentially at the end of the array.
+        state.transactions = [...state.transactions, newTransactions];
       })
       .addCase(postDeduction.rejected, (state, action) => {
-        state.status = 'failed'
-        state.error = `Error occured in financialSlice postDeductionThunk: ${action.payload}`  
-      })
-  }
+        state.status = 'failed';
+        state.error = `Error occured in financialSlice postDeductionThunk: ${action.payload}`;  
+      });
+  },
 });
 
 //export actions from financialSlice:
@@ -74,186 +157,3 @@ export const {updateEarnings, updateDeductions} = financialSlice.actions;
 
 //export reducer:
 export default financialSlice.reducer;
-
-
-
-
-// ORIGINAL CODE:
-
-// const [earningData, setEarningData] = useState({
-//     amount: 0,
-//     source: '',
-//     timestamp: '',
-//     type: 'earning',
-//     medicareTax: 0,
-//     stateTax: 0,
-//     ssiTax: 0,
-//     federalTax: 0,
-
-//   });
-//   const [deductionData, setDeductionData] = useState({
-//     amount: 0,
-//     source: '',
-//     timestamp: '',
-//     type: 'deduction',
-//   });
-
-const postEarning = () => {
-  const token = localStorage.getItem('token');
-
-  setTimeout(()=> {
-
-    fetch('http://localhost:3000/transaction', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`
-      },
-      body: JSON.stringify(earningData),
-    })
-      .then (response => response.json())
-      .then (data => {
-        const stateTax = (Math.abs(data.userTransactionData.stateTax));
-        //DO SOMETHING WITH DATA FROM THE TRANSACTION
-        //UPDATE STATE OF THE CHART 
-
-        const updatedPieChartData = [
-          { id: 'State Tax', label: 'State Tax', value: stateTax },
-          { id: 'Federal Tax', label: 'Federal Tax', value: (Math.abs(data.userTransactionData.fedTax)).toFixed(2) },
-          { id: 'SSI Tax', label: 'SSI Tax', value: (Math.abs(data.userTransactionData.ssiTax)).toFixed(2) },
-          { id: 'Medicare Tax', label: 'Medicare Tax', value: (Math.abs(data.userTransactionData.medicareTax)).toFixed(2) },
-          { id: 'Deductions', label: 'Deductions', value: (Math.abs(data.userTransactionData.businessExpenses)).toFixed(2) },
-          { id: 'Earnings', label: 'Earnings', value: (Math.abs(data.userTransactionData.estimatedIncome)).toFixed(2) },
-        ];
-          
-        setPieChartData(updatedPieChartData);
-        console.log ('Result of transaction coming from Dashboard Container', data);
-
-        //ITERATE THROUGH THE TRANSACTION ARRAY AND UPDATE THE STATE.
-        data.userTransactionData.incomes.forEach((earning) => {
-          //SETTING TRANSACTION DATA
-
-          const newEarningTransaction = {
-            id: transactions.length + 1,
-            description: `Earning | ${earning.source}`,
-            amount: `+$${earning.amount.toFixed(2)}`,
-            medicareTax: `Medicare Tax | ${earning.transMedicare.toFixed(2)}`,
-            stateTax: `State Tax | ${earning.transState.toFixed(2)}`,
-            ssiTax:  `SSI Tax | ${earning.transSSI.toFixed(2)}`,
-            federalTax: `Federal Tax | ${earning.transFed.toFixed(2)}`,
-          
-            // timestamp: currentTime.toISOString(),
-          };
-      
-          setTransactions([...transactions, newEarningTransaction]);
-
-
-        });
-
-
-      })
-      .catch((error) => {
-        console.error('Error while fetching transaction data', error);
-      });
-
-  }, 0);
-
-};
-
-//CHECKING DATA TYPE OF EARNING DATA
-console.log ('CHECKING DATA TYPE OF EARNING DATA', typeof earningData.amount);
-console.log ('CHECKING DATA TYPE OF ded DATA', typeof deductionData.amount);
-
-
-// REALLY HANDLE EVERYTHING SUBMIT - EARNINGS
-
-const handleEarningSubmit = () => {
-
-  //POST REQUEST HERE 
-  const currentTime = new Date();
-  const currentMonth = currentTime.toLocaleString('default', {
-    month: 'short',
-  });
-
-  // TURN STRING TO NUM
-  const earningAmount = parseFloat(earningData.amount);
-
-
-  setEarningData({
-    ...earningData,
-    // timestamp: currentTime.toISOString(),
-    amount: earningAmount
-  });
-
-  console.log ('Value of earning data from DashBoard Container', earningData);
-
-    
-  // UPDATE GROSS
-  setGrossEarnings((prevGrossEarnings) => prevGrossEarnings + earningAmount);
-
-  // CREATE & ADD NEW TRANSACTION
-  const newEarningTransaction = {
-    id: transactions.length + 1,
-    description: `Earning | ${earningData.source}`,
-    amount: `+$${earningAmount.toFixed(2)}`,
-    // timestamp: currentTime.toISOString(),
-  };
-
-  setTransactions([...transactions, newEarningTransaction]);
-
-
-
-  // UPDATE PIE
-  const updatedPieChartData = pieChartData.map((slice) => {
-    if (slice.id === 'Earnings') {
-      return {
-        ...slice,
-        value: slice.value + earningAmount,
-      };
-    }
-    return slice;
-  });
-
-  setPieChartData(updatedPieChartData);
-
-  // UPDATE BAR BUT MAKE SURE ITS THE CURRENT MONTH
-  const updatedBarChartData = barChartData.map((monthData) => {
-    if (monthData.month === currentMonth) {
-      return {
-        ...monthData,
-        earnings: monthData.earnings + earningAmount,
-      };
-    }
-    return monthData;
-  });
-
-  setBarChartData(updatedBarChartData);
-
-  // SAME FOR LINE
-  const updatedLineChartData = lineChartData.map((lineData) => {
-    if (lineData.id === 'Earnings') {
-      return {
-        ...lineData,
-        data: [
-          ...lineData.data,
-          {
-            x: currentMonth,
-            y: lineData.data[lineData.data.length - 1].y + earningAmount,
-          },
-        ],
-      };
-    }
-    return lineData;
-  });
-
-  setLineChartData(updatedLineChartData);
-
-  // RESET FORM
-  setEarningData({
-    amount: 0,
-    source: '',
-    timestamp: '',
-    type:'earning',
-  });
-  closeEarningForm();
-};
